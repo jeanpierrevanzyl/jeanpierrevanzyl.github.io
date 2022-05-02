fe45a94bae2faaf64b74b7b4c05c7a611e30a4a9
Jean-Pierre van Zyl <20706413@sun.ac.za>
2022-05-02 11:17:28 +0200

    Header and new Article
    Added universal header using JS,
    Added WCCI article link and description, will add pdf once camera ready
    version is released

---
+++ header.js
@@ -0,0 +1,33 @@
+document.write('\
+\
+<!-- ======= Header ======= -->\
+    <div class="d-flex flex-column">\
+\
+        <div class="profile">\
+            <a href="index.html"><img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle"></a>\
+            <h1 class="text-light"><a href="index.html">Jean-Pierre van Zyl</a></h1>\
+            <div class="social-links mt-3 text-center">\
+                <a href="https://www.linkedin.com/in/jeanpierrevanzyl/" class="linkedin"><i class="bx bxl-linkedin"></i></a>\
+                <a href="https://www.github.com/jeanpierrevanzyl/" class="github"><i class="bx bxl-github"></i></a>\
+                <a href="https://scholar.google.com/citations?user=SVc16t8AAAAJ" class="scholar"><i class="bx bx-edit"></i></a>\
+            </div>\
+        </div>\
+\
+        <nav class="nav-menu">\
+            <ul>\
+                <li class="active"><a href="#hero"><i class="bx bx-home"></i> <span>Home</span></a></li>\
+                <li><a href="index.html#about"><i class="bx bx-user"></i> <span>About</span></a></li>\
+                <li><a href="index.html#resume"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>\
+                <li><a href="index.html#research"><i class="bx bx-pie-chart-alt-2"></i><span>Research</span></a></li>\
+                <li><a href="index.html#publications"><i class="bx bx-book-open"></i><span>Publications</span></a></li>\
+                <li><a href="index.html#portfolio"><i class="bx bx-book-content"></i> Portfolio</a></li>\
+                <li><a href="index.html#contact"><i class="bx bx-envelope"></i> Contact</a></li>\
+\
+            </ul>\
+        </nav><!-- .nav-menu -->\
+        <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>\
+\
+    </div>\
+<!-- End Header -->\
+\
+');
